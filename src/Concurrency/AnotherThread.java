f4e803d0bb7843850eae5c4f537b2a8ae0aa78cb
marwhals <marjan.mubarok2012@gmail.com>
2023-09-17 21:13:28 +0100

    Add my initial code from the course

---
+++ src/Concurrency/AnotherThread.java
@@ -0,0 +1,11 @@
+package Concurrency;
+
+import static Concurrency.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from another thread.");
+    }
+}
