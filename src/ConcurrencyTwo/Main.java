fb901e9b032b3638bec6f2c9d7a236621e734e54
marwhals <marjan.mubarok2012@gmail.com>
2023-11-15 00:33:41 +0000

    322 - Thread creation and execution

--- src/ConcurrencyTwo/Main.java
+++ src/ConcurrencyTwo/Main.java
@@ -1,6 +1,7 @@
 package ConcurrencyTwo;
 
 
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +14,34 @@ public class Main {
         currentThread.setName("MainGuy");
         currentThread.setPriority(Thread.MAX_PRIORITY); // Priority is from 1 to 10 (max) and is more of a suggestion
         printThreadState(currentThread);
+
+        CustomThread customThread = new CustomThread();
+        customThread.start(); // Run is executed synchoronously from the running thread it is invoked from use start if you want it to be asynchronous
+
+        /*
+            - More flexibility implementing the runnable interface - loosely coupled to the thread class but less control.
+         */
+        Runnable myRunnable = () -> {
+          System.out.println(" 2 ");
+          try {
+              TimeUnit.MILLISECONDS.sleep(250);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+        };
+
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
+
+        for (int i = 1; i<= 3; i ++) {
+            System.out.println(" 0 ");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public static void printThreadState(Thread thread) {
